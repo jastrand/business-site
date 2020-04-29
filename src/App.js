b638c8f09982c6cf2f3912c3299e09d1147225eb
Johanna Åstrand <johanna@johannas-mbp.mynet>
2020-04-29 22:08:27 +0200

    started with listing some of the main components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
+// create a component for navigation top bar
+// create lib for buttons & cards
+// create reusable components for sections 
+// create a header component
+// overview components
+// social media integration with images
+// create a footer
+
 export const App = () => {
   return (
     <div>
